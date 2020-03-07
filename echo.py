97d6d089895b733b457e83f449d3215a79b8db31
Jonathan Jones <jonathan.jones51@yahoo.com>
2020-03-07 01:05:16 -0500

    Not passing one test. Hopefully will get help.

--- echo.py
+++ echo.py
@@ -2,21 +2,65 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "Jonathan Jones"
 
 
 import sys
+import argparse
+import string
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description="Perform transformation on input text."
+    )
+    parser.add_argument(
+        "text", help="text to be manipulated"
+    )
+    parser.add_argument(
+        "-u", "--upper", help="convert text to uppercase",
+        action='store_true'
+    )
+    parser.add_argument(
+        "-l", "--lower", help="convert text to lowercase",
+        action='store_true'
+    )
+    parser.add_argument(
+        "-t", "--title", help="convert text to titlecase",
+        action='store_true'
+    )
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    parser = create_parser()
+    ns = parser.parse_args(args)
+
+    text = ns.text
+    # optional flags
+    title = ns.title
+    upper = ns.upper
+    lower = ns.lower
+
+    # if len(args[1]) >= 3:
+    #     print("All are true")
+    #     print(args)
+    #     print(len(args[1]))
+    if upper:
+        print(text.upper())
+        return text.upper()
+    if lower:
+        print(text.lower())
+        return text.lower()
+    if title:
+        print(string.capwords(text))
+        return string.capwords(text)
+    # if text:
+    #     print(text)
+    #     return text
 
 
 if __name__ == '__main__':
-    pass
+    main(sys.argv[1:])
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,12 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
 import echo
+import unittest
+import subprocess
+import string
+
 
 # Your test case class goes here
+
+
 class TestEcho(unittest.TestCase):
-    pass
+
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
+
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect it's output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+        stdout = stdout.decode('UTF-8')
+        self.assertEquals(stdout, usage)
+
+    def test_upper(self):
+        """ Running the program without arguments should show usage. """
+        parser = echo.create_parser()
+        args = ["hello", "--upper"]
+        ns = parser.parse_args(args)
+        self.assertEquals(ns.upper, True)
+        stdout = echo.main(args)
+        usage = args[0].upper()
+        self.assertEquals(stdout, usage)
+
+    def test_lower(self):
+        """ Running the program without arguments should show usage. """
+        parser = echo.create_parser()
+        args = ["Hello", "--lower"]
+        ns = parser.parse_args(args)
+        self.assertEquals(ns.lower, True)
+        stdout = echo.main(args)
+        usage = args[0].lower()
+        self.assertEquals(stdout, usage)
+
+    def test_title(self):
+        """ Running the program without arguments should show usage. """
+        parser = echo.create_parser()
+        args = ["hello", "--title"]
+        ns = parser.parse_args(args)
+        self.assertEquals(ns.title, True)
+        stdout = echo.main(args)
+        usage = args[0].capitalize()
+        self.assertEquals(stdout, usage)
+
+    def test_all_args(self):
+        """ Running the program without arguments should show usage. """
+        parser = echo.create_parser()
+        args = ["hello", "--lower", "--upper", "--title"]
+        ns = parser.parse_args(args)
+        self.assertEquals(args, True)
+        stdout = echo.main(args)
+        usage = string.capwords(args[0]).lower().upper()
+        self.assertEquals(stdout, usage)
+
+    def test_no_args(self):
+        """ Running the program without arguments should show usage. """
+        parser = echo.create_parser()
+        args = ["Hello"]
+        ns = parser.parse_args(args)
+        self.assertEquals(ns.text, "Hello")
+        stdout = echo.main(args)
+        usage = args[0]
+        self.assertEquals(stdout, usage)
+
 
 if __name__ == '__main__':
     unittest.main()
